ffe83d152ac6c71bcd2c0b2170924a00a92ba9c6
Kevin Hill <kivo360@gmail.com>
2017-06-30 16:45:53 -0400

    finished all RPC calls

--- app/containers/Menu.js
+++ app/containers/Menu.js
@@ -1,10 +1,88 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 
 import style from './Menu.css';
+import * as CounterActions from '../actions/counter';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      pathName: props.router.location.pathname,
+      active: {
+        default: '',
+        send: '',
+        receive: '',
+        transactions: '',
+        about: ''
+      }
+    }
+
+    this.checkStateMenu(props.router.location.pathname);
+  }
+
+  checkStateMenu(pathName) {
+    let aLinks;
+    if (pathName == '/'){
+      aLinks = this.state.active;
+      aLinks['default'] = 'active';
+      aLinks['send'] = '';
+      aLinks['receive'] = '';
+      aLinks['transactions'] = '';
+      aLinks['about'] = '';
+      this.setState({ active: aLinks });
+    }
+
+    if (pathName == '/send'){
+      aLinks = this.state.active;
+      aLinks['default'] = '';
+      aLinks['send'] = 'active';
+      aLinks['receive'] = '';
+      aLinks['transactions'] = '';
+      aLinks['about'] = '';
+      this.setState({ active: aLinks });
+    }
+
+    if (pathName == '/receive') {
+      aLinks = this.state.active;
+      aLinks['default'] = '';
+      aLinks['send'] = '';
+      aLinks['receive'] = 'active';
+      aLinks['transactions'] = '';
+      aLinks['about'] = '';
+      this.setState({ active: aLinks });
+    }
+
+    if (pathName == '/transaction') {
+      aLinks = this.state.active;
+      aLinks['default'] = '';
+      aLinks['send'] = '';
+      aLinks['receive'] = '';
+      aLinks['transactions'] = 'active';
+      aLinks['about'] = '';
+      this.setState({ active: aLinks });
+    }
+
+    if (pathName == '/about') {
+      aLinks = this.state.active;
+      aLinks['default'] = '';
+      aLinks['send'] = '';
+      aLinks['receive'] = '';
+      aLinks['transactions'] = '';
+      aLinks['about'] = 'active';
+      this.setState({ active: aLinks });
+    }
+  }
+
+  componentWillReceiveProps(props){
+    // console.log(props.router.location.pathname);
+    this.checkStateMenu(props.router.location.pathname);
+    this.setState({ pathname: props.router.location.pathname });
+  }
   render() {
     return (
         <div>
@@ -15,52 +93,42 @@ class Menu extends Component {
               </div>
               <ul className="nav navbar-nav">
 
-                <li>
+                <li className={this.state.active.default}>
 
-                  <Link to="/">
-                    <a href="#" className={style.whiteNav}>
-                      Overview
-                    </a>
+                  <Link to="/" style={{ color: 'white' }}>
+                    Overview
                    </Link>
 
                 </li>
 
-                <li className="active">
-                  <Link to="/send">
-                    <a href="#" className={style.whiteNav}>
-                      Send
-                      <span className="sr-only">(current)</span>
-                    </a>
+                <li className={this.state.active.send}>
+                  <Link to="/send" className={style.whiteNav}>
+                    Send
                   </Link>
                 </li>
 
 
-                <li>
+                <li className={this.state.active.receive}>
 
-                  <Link to="/receive">
-                    <a href="#" className={style.whiteNav}>
-                      Recieve
-                    </a>
+                  <Link to="/receive" className={style.whiteNav}>
+                    Recieve
+
                    </Link>
 
                 </li>
 
-                <li>
+                <li className={this.state.active.transactions}>
 
-                  <Link to="/transaction">
-                    <a href="#" className={style.whiteNav}>
-                      Transactions
-                    </a>
-                   </Link>
+                  <Link to="/transaction" className={style.whiteNav}>
+                    Transactions
+                  </Link>
 
                 </li>
 
-                <li>
+                <li className={this.state.active.about}>
 
-                  <Link to="/about">
-                    <a href="#" className={style.whiteNav}>
-                      About
-                    </a>
+                  <Link to="/about" className={style.whiteNav}>
+                    About
                    </Link>
 
                 </li>
@@ -74,4 +142,18 @@ class Menu extends Component {
   }
 }
 
-export default Menu;
+
+function mapStateToProps(state) {
+  return {
+    router: state.router
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(CounterActions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
+
+
+// export default Menu;
